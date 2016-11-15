4fbb5b27170fd6f6f5e12bd66308a54c29abc2df
KhakhulinAndrey <leadtek63@gmail.com>
2016-11-15 13:14:57 +0400

    1 commit

---
+++ src/main/java/usermanager/service/UserService.java
@@ -0,0 +1,20 @@
+package usermanager.service;
+
+import usermanager.model.User;
+
+import java.util.List;
+
+/**
+ * Created by Admin on 14.11.2016.
+ */
+public interface UserService {
+    public void addUser(User user);
+
+    public void updateUser(User user);
+
+    public void removeUser(int id);
+
+    public User getUserById(int id);
+
+    public List<User> listUsers();
+}
